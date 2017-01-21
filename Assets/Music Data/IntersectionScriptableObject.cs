45e0652c833a2bef86710ef630f0185b6bac732e
Wade Gushikuma <wade.gushikuma@gmail.com>
2017-01-21 00:51:09 -0800

    init

---
+++ Assets/Music Data/IntersectionScriptableObject.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[CreateAssetMenu(fileName = "Traffic Data", menuName = "Traffic Intersections", order = 1)]
+public class IntersectionScriptableObject : ScriptableObject {
+	/*public struct Intersection
+	{
+		Vector3[] intersectionPos;
+		bool northOpen, southOpen, eastOpen, westOpen;
+	}
+
+	public Intersection[] intersections;*/
+
+	public Vector3[] intersectionPos;
+
+	// x=north, y=east, z=south, w=west
+	// 0=closed, 1=open
+	public Vector4[] openPathways;
+}
---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControls : MonoBehaviour {
+
+	public GameObject rightBlaster;
+	public GameObject leftBlaster;
+	public GameObject shield;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			leftBlaster.SetActive (true);
+		} else {
+			leftBlaster.SetActive (false);
+		}
+
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			rightBlaster.SetActive (true);
+		} else {
+			rightBlaster.SetActive (false);
+		}
+
+		if (Input.GetKey (KeyCode.Space)) {
+			shield.SetActive (true);
+		} else {
+			shield.SetActive (false);
+		}
+	}
+}
